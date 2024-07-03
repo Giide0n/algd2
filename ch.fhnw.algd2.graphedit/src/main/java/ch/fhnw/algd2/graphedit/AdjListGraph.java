3fe13115b41eb56b6fe842136f362925c6916d50
gideon <gideon.monterosa@gmail.com>
2024-07-03 14:51:24 +0200

    add dfs algorithm

--- ch.fhnw.algd2.graphedit/src/main/java/ch/fhnw/algd2/graphedit/AdjListGraph.java
+++ ch.fhnw.algd2.graphedit/src/main/java/ch/fhnw/algd2/graphedit/AdjListGraph.java
@@ -1,103 +1,125 @@
 package ch.fhnw.algd2.graphedit;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
-public final class AdjListGraph<K> extends AbstractGraph<K> {
-	private static class Vertex<K> {
-		K data;
-		List<Vertex<K>> adjList = new LinkedList<Vertex<K>>();
+public final class AdjListGraph<K> extends AbstractGraph<K> implements GraphAlgorithms.DFS {
 
-		Vertex(K vertex) {
-			data = vertex;
-		}
+    private static class Vertex<K> {
 
-		boolean addEdgeTo(Vertex<K> to) {
-			return (adjList.contains(to)) ? false : adjList.add(to);
-		}
-	}
+        K data;
+        List<Vertex<K>> adjList = new LinkedList<Vertex<K>>();
+        boolean visited = false;
 
-	private Map<K, Vertex<K>> vertices;
+        Vertex(K vertex) {
+            data = vertex;
+        }
 
-	public AdjListGraph() { // default constructor
-		this(false);
-	}
+        boolean addEdgeTo(Vertex<K> to) {
+            return (adjList.contains(to)) ? false : adjList.add(to);
+        }
+    }
 
-	public AdjListGraph(boolean directed) {
-		super(directed);
-		vertices = new HashMap<K, Vertex<K>>();
-	}
+    private Map<K, Vertex<K>> vertices;
 
-	public AdjListGraph(AdjListGraph<K> orig) { // copy constructor
-		// TODO Loeschen Sie folgende Zeile und programmieren Sie
-		// einen Konstruktor, der eine Kopie von orig erstellt.
-		this(false);
-	}
+    public AdjListGraph() { // default constructor
+        this(false);
+    }
 
-	@Override
-	public boolean addVertex(K vertex) {
-		if (vertex != null && !vertices.containsKey(vertex)) {
-			// TODO Einfuegen des neuen Knotens in HashMap
-			return true;
-		} else {
-			return false;
-		}
-	}
+    public AdjListGraph(boolean directed) {
+        super(directed);
+        vertices = new HashMap<K, Vertex<K>>();
+    }
 
-	@Override
-	public boolean addEdge(K from, K to) {
-		Vertex<K> vf = vertices.get(from);
-		Vertex<K> vt = vertices.get(to);
-		if (vf != null && vt != null && !vf.adjList.contains(vt)) {
-			// TODO Kante einfuegen, es muss dabei unterschieden werden, ob der
-			// Graph gerichtet ist oder nicht.
-			return true;
-		} else {
-			return false;
-		}
-	}
+    public AdjListGraph(AdjListGraph<K> orig) { // copy constructor
+        // TODO Loeschen Sie folgende Zeile und programmieren Sie
+        // einen Konstruktor, der eine Kopie von orig erstellt.
+        this(false);
+    }
 
-	@Override
-	public boolean removeEdge(K from, K to) {
-		Vertex<K> vf = vertices.get(from);
-		Vertex<K> vt = vertices.get(to);
-		if (vf != null && vt != null && vf.adjList.contains(vt)) {
-			// TODO Kante loeschen, es muss dabei unterschieden werden, ob der
-			// Graph gerichtet ist oder nicht.
-			return true;
-		} else {
-			return false;
-		}
-	}
+    @Override
+    public boolean addVertex(K id) {
+        if (id != null && !vertices.containsKey(id)) {
+            // TODO Einfuegen des neuen Knotens in HashMap
+            vertices.put(id, new Vertex<>(id));
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	@Override
-	public int getNofVertices() {
-		// TODO Ersetzen Sie die folgende Zeile durch eine effizientere
-		// Implementation
-		return super.getNofVertices();
-	}
+    @Override
+    public boolean addEdge(K fromId, K toId) {
+        Vertex<K> vf = vertices.get(fromId);
+        Vertex<K> vt = vertices.get(toId);
+        if (vf != null && vt != null && !vf.adjList.contains(vt)) {
+            return vf.addEdgeTo(vt);
+        } else {
+            return false;
+        }
+    }
 
-	@Override
-	public int getNofEdges() {
-		// TODO Ersetzen Sie die folgende Zeile durch eine effizientere
-		// Implementation
-		return super.getNofEdges();
-	}
+    @Override
+    public boolean removeEdge(K from, K to) {
+        Vertex<K> vf = vertices.get(from);
+        Vertex<K> vt = vertices.get(to);
+        if (vf != null && vt != null && vf.adjList.contains(vt)) {
+            vf.adjList.remove(vt);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	@Override
-	public Set<K> getVertices() {
-		return new HashSet<K>(vertices.keySet());
-	}
+    @Override
+    public int getNofVertices() {
+        // TODO Ersetzen Sie die folgende Zeile durch eine effizientere
+        // Implementation
+        return super.getNofVertices();
+    }
 
-	@Override
-	public Set<K> getAdjacentVertices(K vertex) {
-		Set<K> set = new HashSet<K>();
-		// TODO Alle data-Objekte, die in den benachbarten
-		// Knoten gespeichert sind, in set einf�gen
-		return set;
-	}
+    @Override
+    public int getNofEdges() {
+        // TODO Ersetzen Sie die folgende Zeile durch eine effizientere
+        // Implementation
+        return super.getNofEdges();
+    }
 
-	@Override
-	public Object clone() {
-		return new AdjListGraph<K>(this);
-	}
+    @Override
+    public Set<K> getVertices() {
+        return new HashSet<K>(vertices.keySet());
+    }
+
+    @Override
+    public Set<K> getAdjacentVertices(K vertex) {
+        Set<K> set = vertices.get(vertex).adjList.stream().map(v -> v.data)
+            .collect(Collectors.toSet());
+        ;
+        return set;
+    }
+
+    @Override
+    public Object clone() {
+        return new AdjListGraph<K>(this);
+    }
+
+    @Override
+    public Graph traverse(Object startVertex) {
+        vertices.values().forEach(v -> v.visited = false);
+
+        dfs(vertices.get(startVertex));
+
+        return this;
+    }
+
+    private void dfs(Vertex<K> vertex) {
+        if(!vertex.visited) {
+            vertex.visited = true;
+            System.out.println(vertex.data);
+
+            for (Vertex<K> v: vertex.adjList) {
+                dfs(v);
+            }
+        }
+    }
 }
